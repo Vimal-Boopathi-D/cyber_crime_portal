0e41ce6e301c4244be955e505231fd7208673c5a
Dharshini-Finytive <dharshini.sibiyal@finytive.com>
2026-02-02 13:10:10 +0530

    removed ai Management

--- src/main/java/com/cyber/portal/aiManagement/service/RAGService.java
+++
@@ -1,9 +0,0 @@
-package com.cyber.portal.aiManagement.service;
-
-import com.cyber.portal.sharedResources.enums.IncidentStatus;
-
-public interface RAGService {
-    String chatUsingRag(String message);
-    String explainIncidentStatus(IncidentStatus status);
-
-}
