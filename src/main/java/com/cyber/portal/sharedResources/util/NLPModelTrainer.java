e5660b3bf83033aff91759956aef542864d3cdaf
Vimal-Boopathi-Finytive <vimal.boopathi@finytive.com>
2026-04-06 10:19:27 +0530

    modified nlp

--- src/main/java/com/cyber/portal/sharedResources/util/NLPModelTrainer.java
+++ src/main/java/com/cyber/portal/sharedResources/util/NLPModelTrainer.java
@@ -1,34 +1,50 @@
 package com.cyber.portal.sharedResources.util;
 
-import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
 import weka.classifiers.meta.FilteredClassifier;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.tokenizers.NGramTokenizer;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
 public class NLPModelTrainer {
 
+    private static final String DATASET_PATH = "complaints.arff";
+    private static final String MODEL_PATH = "complaint.model";
+
     public static void main(String[] args) throws Exception {
 
-        DataSource source = new DataSource("complaints.arff");
+        DataSource source = new DataSource(DATASET_PATH);
         Instances dataset = source.getDataSet();
-        dataset.setClassIndex(dataset.numAttributes() - 1);
+
+        if (dataset.classIndex() == -1) {
+            dataset.setClassIndex(dataset.numAttributes() - 1);
+        }
 
         StringToWordVector filter = new StringToWordVector();
+
         filter.setTFTransform(true);
         filter.setIDFTransform(true);
         filter.setLowerCaseTokens(true);
 
-        NaiveBayes nb = new NaiveBayes();
+        NGramTokenizer tokenizer = new NGramTokenizer();
+        tokenizer.setNGramMinSize(1);
+        tokenizer.setNGramMaxSize(2);
+
+        filter.setTokenizer(tokenizer);
+        filter.setWordsToKeep(5000);
+        filter.setStopwordsHandler(new weka.core.stopwords.Rainbow());
+
+        SMO svm = new SMO();
 
         FilteredClassifier fc = new FilteredClassifier();
         fc.setFilter(filter);
-        fc.setClassifier(nb);
+        fc.setClassifier(svm);
 
         fc.buildClassifier(dataset);
 
-        weka.core.SerializationHelper.write("complaint.model", fc);
+        weka.core.SerializationHelper.write(MODEL_PATH, fc);
 
-        System.out.println("Model trained and saved.");
+        System.out.println(" Model trained and saved at: " + MODEL_PATH);
     }
 }
